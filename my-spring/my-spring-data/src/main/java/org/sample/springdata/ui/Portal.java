4009d96e855c44509045ac51bc5ea852a1561220
peter <peterdnight@github.com>
2022-02-22 13:55:56 -0500

    updated imports

--- my-spring/my-spring-data/src/main/java/org/sample/springdata/ui/Portal.java
+++ my-spring/my-spring-data/src/main/java/org/sample/springdata/ui/Portal.java
@@ -6,7 +6,6 @@ import java.time.format.DateTimeFormatter ;
 import org.sample.springdata.utils.Utils ;
 import org.slf4j.Logger ;
 import org.slf4j.LoggerFactory ;
-import org.springframework.cache.annotation.CacheConfig ;
 import org.springframework.stereotype.Controller ;
 import org.springframework.ui.Model ;
 import org.springframework.web.bind.annotation.GetMapping ;
